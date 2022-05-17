97e821319431699c0bf782997a2f7054b22f9a20
chakradhar <chakradharreddy9642@gmail.com.com>
2022-05-17 10:13:38 +0530

    i am working with Git

---
+++ springboot-imdday42/src/main/java/com/jobiak/imd/SpringbootImdday42Application.java
@@ -0,0 +1,17 @@
+package com.jobiak.imd;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootImdday42Application {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootImdday42Application.class, args);
+		
+
+	
+	
+	}
+	
+	}
